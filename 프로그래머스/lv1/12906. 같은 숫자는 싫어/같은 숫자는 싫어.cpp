3fcba355becff6f7a4eb4fb49a0c2f874d9dacbf
getCurrentThread <wak8835@gmail.com>
2026-03-19 22:21:00 +0900

    전체 코드 업로드 -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for (int i = 0; i < arr.size(); i++) {
+        if (i == 0 || arr[i] != arr[i-1]) {
+            answer.push_back(arr[i]);
+        }
+    }
+    return answer;
+}
